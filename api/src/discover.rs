3ab592bd8a9fc2c79a5a9ec16404cb34dcd555c7
TabulateJarl8 <tabulatejarl8@gmail.com>
2025-01-31 14:14:07 -0500

    better error handling

--- api/src/discover.rs
+++ api/src/discover.rs
@@ -1,4 +1,4 @@
-use axum::{extract::Path, Json};
+use axum::{extract::Path, response::IntoResponse, Json};
 
 use crate::{
     frontend_models::{FrontendMovieDetails, FrontendMovieList},
@@ -11,33 +11,53 @@ use crate::{
     },
 };
 
-pub async fn fetch_trending() -> Json<FrontendMovieList> {
-    let client = TMDBClient::new(std::env::var("TMDB_API_KEY").unwrap());
-    Json(
-        MovieListTrendingRequest::new()
-            .fetch(&client)
-            .await
-            .expect("handle errors later"),
-    )
+pub struct ApiFetchError {
+    status_code: String,
 }
 
-pub async fn fetch_now_playing() -> Json<FrontendMovieList> {
-    let client = TMDBClient::new(std::env::var("TMDB_API_KEY").unwrap());
-    Json(
-        MovieListNowPlayingRequest::new()
-            .fetch(&client)
-            .await
-            .expect("handle errors later"),
-    )
+impl IntoResponse for ApiFetchError {
+    fn into_response(self) -> axum::response::Response {
+        (
+            axum::http::StatusCode::INTERNAL_SERVER_ERROR,
+            format!(
+                "Error while fetching data from TMDB. Got status: {}",
+                self.status_code
+            ),
+        )
+            .into_response()
+    }
+}
+impl From<reqwest::Error> for ApiFetchError {
+    fn from(value: reqwest::Error) -> Self {
+        Self {
+            status_code: match value.status() {
+                Some(v) => v.to_string(),
+                None => "N/A".to_string(),
+            },
+        }
+    }
 }
 
-pub async fn fetch_movie_details(Path(movie_id): Path<u64>) -> Json<FrontendMovieDetails> {
+pub async fn fetch_trending() -> Result<Json<FrontendMovieList>, ApiFetchError> {
     let client = TMDBClient::new(std::env::var("TMDB_API_KEY").unwrap());
-    Json(
+    Ok(Json(MovieListTrendingRequest::new().fetch(&client).await?))
+}
+
+pub async fn fetch_now_playing() -> Result<Json<FrontendMovieList>, ApiFetchError> {
+    let client = TMDBClient::new(std::env::var("TMDB_API_KEY").unwrap());
+    Ok(Json(
+        MovieListNowPlayingRequest::new().fetch(&client).await?,
+    ))
+}
+
+pub async fn fetch_movie_details(
+    Path(movie_id): Path<u64>,
+) -> Result<Json<FrontendMovieDetails>, ApiFetchError> {
+    let client = TMDBClient::new(std::env::var("TMDB_API_KEY").unwrap());
+    Ok(Json(
         MovieDetailsRequest::new()
             .append_to_response("credits")
             .fetch(&client, movie_id)
-            .await
-            .expect("handle errors later"),
-    )
+            .await?,
+    ))
 }
